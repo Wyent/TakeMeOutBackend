9c751086e1ffa4f3d965d3103ffd1fb425fe1064
Wyent <dwyent@gmail.com>
2022-04-17 22:00:58 -0500

    added cors to main.py

--- dateScraper/main.py
+++ dateScraper/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from Scraper import Scraper
-
 
 app = FastAPI()  # create app instance
 dates = Scraper()
+
+# allowed domains
+origins = [
+    "https://vast-reaches-22877.herokuapp.com",
+    "https://powerful-taiga-30378.herokuapp.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=[]
+)
 
 
 # define route, use / for landing page
@@ -13,4 +26,3 @@ async def read_item(lat: float, lon: float):
     # return python dictionary, auto converted to json
     # return dates.get_dates_meetup(lat, lon)
     return dates.get_dates_tripbuzz(lat, lon)
-
